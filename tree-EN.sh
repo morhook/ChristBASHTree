207167a154dd4b7d25f8a1e99e7468b95adafed7
morhook <cruz.fernandez@gmail.com>
2024-12-21 11:18:12 -0300

    Make tree recenter when window is resized
    Specially useful for making decorations on big monitors

--- tree-EN.sh
+++ tree-EN.sh
@@ -1,41 +1,47 @@
 #!/bin/bash
 trap "tput reset; tput cnorm; exit" 2
-clear
-tput civis
-lin=2
-col=$(($(tput cols) / 2))
-c=$((col-1))
-est=$((c-2))
-color=0
-tput setaf 2; tput bold
+DRAW_TREE () {
+    clear
+    tput civis
+    lin=2
+    col=$(($(tput cols) / 2))
+    c=$((col-1))
+    est=$((c-2))
+    color=0
+    tput setaf 2; tput bold
 
-# Tree
-for ((i=1; i<20; i+=2))
-{
-    tput cup $lin $col
-    for ((j=1; j<=i; j++))
+    # Tree
+    for ((i=1; i<20; i+=2))
     {
-        echo -n \*
+        tput cup $lin $col
+        for ((j=1; j<=i; j++))
+        {
+            echo -n \*
+        }
+        let lin++
+        let col--
     }
-    let lin++
-    let col--
+
+    tput sgr0; tput setaf 3
+
+    # Trunk
+    for ((i=1; i<=2; i++))
+    {
+        tput cup $((lin++)) $c
+        echo 'mWm'
+    }
+    new_year=$(date +'%Y')
+    let new_year++
+    tput setaf 1; tput bold
+    tput cup $lin $((c - 6)); echo MERRY CHRISTMAS
+    tput cup $((lin + 1)) $((c - 10)); echo And lots of CODE in $new_year
+    let c++
+    k=1
 }
 
-tput sgr0; tput setaf 3
+DRAW_TREE
 
-# Trunk
-for ((i=1; i<=2; i++))
-{
-    tput cup $((lin++)) $c
-    echo 'mWm'
-}
-new_year=$(date +'%Y')
-let new_year++
-tput setaf 1; tput bold
-tput cup $lin $((c - 6)); echo MERRY CHRISTMAS
-tput cup $((lin + 1)) $((c - 10)); echo And lots of CODE in $new_year
-let c++
-k=1
+trap DRAW_TREE SIGWINCH
 
 # Lights and decorations
 while true; do
